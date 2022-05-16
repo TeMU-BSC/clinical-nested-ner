0a93a46394b284b3a8df2f5a95ba918807a40916
Casimiro <c.casimiro88@gmail.com>
2022-05-16 15:26:28 +0200

    add prediction script

--- language-models/cte-amd/ner_joint_labeling.sh
+++ language-models/cte-amd/ner_joint_labeling.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Script to run joint labelling experiments
 
+MODELS='/gpfs/projects/bsc88/projects/bio_eval/power9/models/mbert-cased /gpfs/projects/bsc88/projects/bne/eval_amd/models/beto /gpfs/projects/bsc88/projects/bio_eval/power9/models/bne-base /gpfs/projects/bsc88/projects/bio_eval/power9/models/BETO_Galen_ckpt /gpfs/projects/bsc88/projects/bio_eval/power9/models/mBERT_Galen_ckpt'
 for seed in 1 2 3; do
 for learning_rate in 5e-5 1e-5 5e-6 1e-6; do
-for model_name in /gpfs/projects/bsc88/projects/bio_eval/power9/models/biomedical-clinical-swm-vocab-50k /gpfs/projects/bsc88/projects/bio_eval/power9/models/biomedical-swm-vocab-50k; do
-for dataset_name in wl pharmaconer clinical_trials; do
+for model_name in $MODELS; do
+for dataset_name in wl clinical_trials; do
   echo "running exp $seed $learning_rate $model_name $dataset_name $gradient_accumulation"
   sbatch /gpfs/projects/bsc88/temutauro/ccasimiro/clinical-nested-ner/language-models/cte-amd/ner.sh $seed $model_name $dataset_name $learning_rate
 done
