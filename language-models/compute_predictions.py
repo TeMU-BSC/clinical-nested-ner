7c92235e40ba33c8f270576259277765882150fa
Casimiro <c.casimiro88@gmail.com>
2022-05-17 12:01:11 +0200

    add aggregation_strategy

--- language-models/compute_predictions.py
+++ language-models/compute_predictions.py
@@ -13,13 +13,16 @@ if __name__ == '__main__':
     path = Path(runs_dir)
     pretrained_models = [os.path.dirname(model) for model in path.rglob('*pytorch_model.bin')]
     testsets = ['wl', 'clinical_trials']
+    aggregation_strategy='first'
 
     for model in tqdm(pretrained_models):
         print(model)
         pipe = pipeline('token-classification', model, device=0)
         model_name = re.findall('.*runs/(.*)', model)[0]
 
-        with open(os.path.join(model, 'preds_with_labels.conll'), 'w') as fn:
+        preds_dir = f'/home/ccasimiro/ccasimiro/clinical-nested-ner/language-models/cte-amd/preds/{model_name}'
+        os.makedirs(preds_dir, exist_ok=True)
+        with open(os.path.join(preds_dir,'preds_with_labels.conll'), 'w') as fn:
             for testset in tqdm(testsets):
                 print(testset)
                 dataset = load_dataset('/home/ccasimiro/ccasimiro/clinical-nested-ner/datasets/load_ner_iob2.py', testset, 
@@ -27,7 +30,7 @@ if __name__ == '__main__':
                 
                 for words, labels in tqdm(zip(dataset['test']['tokens'], dataset['test']['ner_tags'])):
                     sentence = ' '.join(words)
-                    prediction = pipe(sentence)
+                    prediction = pipe(sentence, aggregation_strategy=aggregation_strategy)
                     start_word = 0
                     for word, label in zip(words, labels):
                         label_name =  dataset['test'].features['ner_tags'].feature.names[label]
